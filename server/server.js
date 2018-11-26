8e5f0f6cdc649a819d5ed4d30b05b94c0963b8fc
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-11-26 10:04:36 -0800

    added mongoose and mongodb configurations

--- server/server.js
+++ server/server.js
@@ -1,15 +1,27 @@
-const path = require('path');
-const express = require('express');
+const path = require("path");
+const express = require("express");
+const mongoose = require("mongoose");
+
+/* Express App */
 const app = express();
-const publicPath = path.join(__dirname, '..', 'public');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
-app.use(express.static(publicPath));
+/* DB Config & Mongoose to Mongo connection */
+const db = require("./config/keys").mongoURI;
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB connected Successfully!"))
+  .catch(e => console.log(`Error while connecting to MongoDB ${e}`));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(publicPath, 'index.html'));
-});
+// Pointing public folder
+const publicPath = path.join(__dirname, "..", "public");
+// app.use(express.static(publicPath));
+
+// Serve React app for all routes
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(publicPath, "index.html"));
+// });
 
 app.listen(port, () => {
-  console.log('Server is up!');
+  console.log("Server is up!");
 });
