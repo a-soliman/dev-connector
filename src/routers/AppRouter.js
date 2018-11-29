a5b615f64d7a68b4a919edeab1b17df29869acf9
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-11-29 10:35:25 -0800

    made use of the layout components

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,22 +1,28 @@
-import React from 'react';
-import { Router, Route, Switch, Link, NavLink } from 'react-router-dom';
-import createHistory from 'history/createBrowserHistory';
-import DashboardPage from '../components/DashboardPage';
-import NotFoundPage from '../components/NotFoundPage';
-import LoginPage from '../components/LoginPage';
-import PrivateRoute from './PrivateRoute';
-import PublicRoute from './PublicRoute';
+import React from "react";
+import { Router, Route, Switch, Link, NavLink } from "react-router-dom";
+import createHistory from "history/createBrowserHistory";
+/* LAYOUT COMPONENTS */
+import Navbar from "../components/layout/Navbar";
+import Footer from "../components/layout/Footer";
+
+import DashboardPage from "../components/DashboardPage";
+import NotFoundPage from "../components/NotFoundPage";
+import LoginPage from "../components/LoginPage";
+import PrivateRoute from "./PrivateRoute";
+import PublicRoute from "./PublicRoute";
 
 export const history = createHistory();
 
 const AppRouter = () => (
   <Router history={history}>
     <div>
+      <Navbar />
       <Switch>
         <PublicRoute path="/" component={LoginPage} exact={true} />
         <PrivateRoute path="/dashboard" component={DashboardPage} />
         <Route component={NotFoundPage} />
       </Switch>
+      <Footer />
     </div>
   </Router>
 );
