79ec7e7e70e9ee01f9d3c583812bd2baf147c3a3
a-soliman <ahmed.soliman@programmer.net>
2018-11-30 11:42:32 -0800

    coded register action

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -1,5 +1,16 @@
-export default (state = {}, action) => {
+const initalState = {
+  isAuthenticated: false,
+  user: {}
+};
+
+export default (state = initalState, action) => {
   switch (action.type) {
+
+    case 'REGISTER':
+      return {
+        user: action.userData
+      };
+
     case 'LOGIN':
       return {
         uid: action.uid
