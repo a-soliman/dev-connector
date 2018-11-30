04a7df1a83bad4d2ff233ae4fae2d5d47a259c78
a-soliman <ahmed.soliman@programmer.net>
2018-11-30 14:27:27 -0800

    completed logout reducer

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -1,3 +1,6 @@
+import { SET_CURRENT_USER } from '../actions/types';
+import isEmpty from '../validation/is-empty';
+
 const initalState = {
   isAuthenticated: false,
   user: {}
@@ -11,12 +14,24 @@ export default (state = initalState, action) => {
         user: action.userData
       };
 
+    case 'SET_CURRENT_USER':
+      return {
+        ...state,
+        isAuthenticated: !isEmpty(action.payload),
+        user: action.payload
+      };
+
+
     case 'LOGIN':
       return {
         uid: action.uid
       };
     case 'LOGOUT':
-      return {};
+      return {
+        ...state,
+        isAuthenticated: false,
+        user: {}
+      };
     default:
       return state;
   }
