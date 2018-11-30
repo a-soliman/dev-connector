75239cb9f00616fc1e184a9f828b7deb5c2b9d52
a-soliman <ahmed.soliman@programmer.net>
2018-11-30 11:42:57 -0800

    added an action for register

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,4 +1,20 @@
-import { firebase, googleAuthProvider } from '../firebase/firebase';
+// import { firebase, googleAuthProvider } from '../firebase/firebase';
+import Axios from "axios";
+import { GET_ERRORS } from './types';
+import { history } from "../routers/AppRouter";
+
+export const register = (userData) => (dispatch) => {
+
+  /* SEND TO THE API */
+  Axios.post("/api/users/register", userData)
+    .then(res => history.push('/login'))
+    .catch(err => {
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    });
+};
 
 export const login = (uid) => ({
   type: 'LOGIN',
