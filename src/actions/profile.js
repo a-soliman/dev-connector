163457c637d16326e59558a0ae69fde37e6b5f04
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-12-05 10:51:08 -0800

    create profile action added

--- src/actions/profile.js
+++ src/actions/profile.js
@@ -1,10 +1,13 @@
 import axios from "axios";
+import { history } from "../routers/AppRouter";
 import {
   Get_PROFILE,
   PROFILE_LOADING,
+  CREATE_PROFILE,
   GET_ERRORS,
   CLEAR_CURRENT_PROFILE
 } from "./types";
+import CreateProfile from "../components/create-profile/CreateProfile";
 
 /* GET CURRENT PROFILE */
 export const getCurrentProfile = () => dispatch => {
@@ -36,3 +39,24 @@ export const setProfileLoading = () => ({
 export const clearCurrentProfile = () => ({
   type: CLEAR_CURRENT_PROFILE
 });
+
+/* CREATE PROFILE */
+export const createProfile = profileData => dispatch => {
+  dispatch(setProfileLoading());
+  axios
+    .post("/api/profile", profileData)
+    .then(res => {
+      dispatch({
+        type: CREATE_PROFILE,
+        payload: res.data,
+        loading: false
+      });
+      history.push("/dashboard");
+    })
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
