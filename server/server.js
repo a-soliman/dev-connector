f0576fe704e039d59b412dc370d1be56bb17ace1
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-12-11 08:03:55 -0800

    deployment debugging

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const posts = require("./routes/api/posts");
 
 /* EXPRESS APP */
 const app = express();
-const port = process.env.PORT || 5555;
+const port = process.env.PORT || 5001;
 
 /* BODY PARSER CONFIGURATIONS */
 app.use(bodyParser.urlencoded({ extended: false }));
